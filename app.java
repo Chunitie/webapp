1481942d861b71286ad2936f6fd5f2bbb0684a9c
Chunitie <118619093+Chunitie@users.noreply.github.com>
2022-11-21 03:43:04 -0500

    Update app.java

--- app.java
+++ app.java
@@ -2,4 +2,6 @@
 #please do not hardcode
 #make use of variable and dynamic variables
 #author:Bemi A
-
+#This is a from end application
+#This is another back end application
+#And you will be alright 
